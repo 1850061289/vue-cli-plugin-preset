63abeaf28f28bfe332d199e9fd50b1aafc14b19c
fangwei <fangwei@ucfgroup.com>
2019-06-13 20:46:23 +0800

    update

--- generator.js
+++ generator.js
@@ -3,18 +3,28 @@ module.exports = (api, options, rootOptions) => {
   api.extendPackage({
     dependencies: {
       'axios': '^0.18.0'
+    },
+    scripts:{
+      "test_build": "vue-cli-service build --mode test_production"
     }
   })
 
   // 删除 vue-cli3 默认目录
   api.render(files => {
     Object.keys(files)
-      .filter(path => path.startsWith('src/') || path.startsWith('public/'))
+      .filter(path => path.startsWith('src/'))
       .forEach(path => delete files[path])
+
+    Object.keys(files).forEach(path => {
+      if(!path.startsWith('public')) {
+        delete files[path]
+      }
+      
+    })
   })
 
   // 生成项目文件
-  // api.render('./template')
+  api.render('./template')
 
   // 屏蔽 generator 之后的文件写入操作
   // api.onCreateComplete(() => {
