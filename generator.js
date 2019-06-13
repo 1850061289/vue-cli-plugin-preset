7b384790b01906390f3346e30c74969b6a49643c
fangwei <fangwei@ucfgroup.com>
2019-06-13 18:16:21 +0800

    test

--- generator.js
+++ generator.js
@@ -7,11 +7,11 @@ module.exports = (api, options, rootOptions) => {
   })
 
   // 删除 vue-cli3 默认目录
-  // api.render(files => {
-  //   Object.keys(files)
-  //     .filter(path => path.startsWith('src/') || path.startsWith('public/'))
-  //     .forEach(path => delete files[path])
-  // })
+  api.render(files => {
+    Object.keys(files)
+      .filter(path => path.startsWith('src/') || path.startsWith('public/'))
+      .forEach(path => delete files[path])
+  })
 
   // 生成项目文件
   // api.render('./template')
