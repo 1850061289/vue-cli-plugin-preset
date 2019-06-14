3d8e41eb0ecf0370574d321afecc201eb6fb8071
fangwei <fangwei@ucfgroup.com>
2019-06-14 15:54:49 +0800

    给出代理配置的模板注释

--- template/vue.config.js
+++ template/vue.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   devServer: {
-    
+    proxy: {
+      // 例子
+      // '/wine': {
+      //   target: 'http://alipay.test.wanmengjun.com/',
+      //   changeOrigin: true
+      // },
+    }
+
   }
 
 }
