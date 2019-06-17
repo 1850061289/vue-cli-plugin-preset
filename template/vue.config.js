018ca26e9f4aeb91c202e262cee979d81dcb355b
fangwei <fangwei@ucfgroup.com>
2019-06-17 14:54:27 +0800

    update vue.config.js

--- template/vue.config.js
+++ template/vue.config.js
@@ -1,13 +1,16 @@
 module.exports = {
   devServer: {
     // proxy: {
-      // 例子
-      // '/wine': {
-      //   target: 'http://alipay.test.wanmengjun.com/',
-      //   changeOrigin: true
-      // },
+    // 例子
+    // '/wine': {
+    //   target: 'http://alipay.test.wanmengjun.com/',
+    //   changeOrigin: true
+    // },
     // }
 
-  }
+  },
 
+  outputDir: "dist_pre",
+  assetsDir: "static",
+  productionSourceMap: false
 }
